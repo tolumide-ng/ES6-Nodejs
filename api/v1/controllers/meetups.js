b6fa9b2d59b08e557bb72476751e3b12b22576b6
Tolumide <tolumideshopein@gmail.com>
2018-12-28 17:11:53 +0100

    Added error routes on app.js(ok)

---
+++ api/v1/controllers/meetups.js
@@ -0,0 +1,3 @@
+/* function upcoming(req, res) {
+    
+} */
--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 import express from 'express';
-/* import meetupRoutes from './api/v1/routes/meetups' */
-
+/* import meetupRoutes from './api/v1/routes/meetups';
+ */
 const app = express();
 
-app.use((req, res) => {
-  res.status(200).json({
-    message: 'first confirmation',
-  });
+/* meetupRoutes(app); */
+
+app.use(('/'), (req, res, next) => {
+    res.status(200).json({
+        message: 'Haha! you got me'
+    })
+})
+
+app.use((req, res, next) => {
+  const error = new Error('Bad Request, Route not found');
+  error.status = 400;
+  next(error);
 });
 
+app.use((error, req, res) => {
+  res.status(error.statusCode || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 export default app;

925dbc4718989d54982ec20af48d112848ba75a1
Tolumide <tolumideshopein@gmail.com>
2019-01-01 16:58:09 +0100

    lint meetup.js of routes

--- api/v1/routes/meetup.js
+++ api/v1/routes/meetup.js
@@ -2,26 +2,26 @@ import meetups from '../controllers/meetups';
 
 
 const routes = (app) => {
-	app.route('/v1/meetups')
-		.get(meetups.findAll);
+  app.route('/v1/meetups')
+    .get(meetups.findAll);
 
-	app.route('/v1/meetups')
-		.post(meetups.createMeetup);
+  app.route('/v1/meetups')
+    .post(meetups.createMeetup);
 
-	app.route('/v1/meetups/upcoming')
-		.get(meetups.allUpcomings);
+  app.route('/v1/meetups/upcoming')
+    .get(meetups.allUpcomings);
 
-	app.route('/v1/meetups/:meetupId')
-		.get(meetups.findOne);
+  app.route('/v1/meetups/:meetupId')
+    .get(meetups.findOne);
 
-	app.route('/v1/meetups/delete/:meetupId')
-		.delete(meetups.deleteMeetup);
+  app.route('/v1/meetups/delete/:meetupId')
+    .delete(meetups.deleteMeetup);
 
-	app.route('/v1/meetups/attendees/:meetupId')
-		.get(meetups.findAttending);
+  app.route('/v1/meetups/attendees/:meetupId')
+    .get(meetups.findAttending);
 
-	app.route('/v1/meetups/edit')
-		.get(meetups.edit);
+  app.route('/v1/meetups/edit')
+    .get(meetups.edit);
 };
 
 export default routes;

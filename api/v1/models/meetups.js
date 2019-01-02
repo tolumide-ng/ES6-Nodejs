3635ea3aac56ce6c24333e82a28fc292f6651270
Tolumide <tolumideshopein@gmail.com>
2019-01-02 06:16:04 +0100

    all meetup routes respond to test

--- api/v1/models/meetups.js
+++ api/v1/models/meetups.js
@@ -3,76 +3,76 @@ import uuid from 'uuid';
 
 
 class Meetup {
-	constructor() {
-		this.meetups = [];
-		this.attending = [];
-	}
+  constructor() {
+    this.meetups = [];
+    this.attending = [];
+  }
 
-	create(data) {
-		const meetup = {
-			meetupId: uuid.nv4(),
-			createdOn: moment.now(),
-			location: data.location,
-			images: data.images || '',
-			topic: data.topic,
-			happeningOn: data.happeningOn, //MUST BE IN THR ORDER mm/dd/yyy
-			tags: data.tags,
-			attending: this.attending.length
-		}
-		this.meetups.push(meetup);
-		return meetup;
-	}
+  create(data) {
+    const meetup = {
+      meetupId: uuid.nv4(),
+      createdOn: moment.now(),
+      location: data.location,
+      images: data.images || '',
+      topic: data.topic,
+      happeningOn: data.happeningOn, // MUST BE IN THR ORDER mm/dd/yyy
+      tags: data.tags,
+      attending: this.attending.length,
+    };
+    this.meetups.push(meetup);
+    return meetup;
+  }
 
-	//NEW HERE
-	attend(data) {
-		const attend = {
-			meetupId: data.meetupId,
-			userId: data.userId
-		}
-		this.attending.push(attend);
-		return attend;
-	}
+  // NEW HERE
+  attend(data) {
+    const attend = {
+      meetupId: data.meetupId,
+      userId: data.userId,
+    };
+    this.attending.push(attend);
+    return attend;
+  }
 
-	//NEW HERE
-	findAttendingMeetup(meetupId) {
-		return this.attending.filter(attends => attends.meetupId === meetupId);
-	}
+  // NEW HERE
+  findAttendingMeetup(meetupId) {
+    return this.attending.filter(attends => attends.meetupId === meetupId);
+  }
 
-	getAll() {
-		return this.meetups;
-	}
+  getAll() {
+    return this.meetups;
+  }
 
-	getOne(meetupId) {
-		return this.meetups.find(meetup => meetup.meetupId === meetupId);
-	}
+  getOne(meetupId) {
+    return this.meetups.find(meetup => meetup.meetupId === meetupId);
+  }
 
-	upcomings() {
-		const upcoming = this.meetups.filter(meetup => new Date(meetup.happeningOn) > new Date());
-		return upcoming;
-	}
+  upcomings() {
+    const upcoming = this.meetups.filter(meetup => new Date(meetup.happeningOn) > new Date());
+    return upcoming;
+  }
 
-	//NEW HERE --REMEMBER TO ADD THE EDIT METHOD FOR THE MEETUP
-	edit(data) {
-		//use the getOne method to find the meetup
-		const theMeetup = this.getOne(data.meetupId);
-		const index = this.meetups.indexOf(theMeetup);
-		//Now you can do whatever you so wish on controllers endpoint
-		this.meetups[index].images = data['images']|| theMeetup.images;
-		this.meetups[index].topic = data['topic']|| theMeetup.topic;
-		this.meetups[index].location = data['location']||theMeetup.location;
-		this.meetups[index].happeningOn = data['happeningOn']||theMeetup.happeningOn;
-		this.meetups[index].tags = data['tags']|| theMeetup.tags;
-	}
+  // NEW HERE --REMEMBER TO ADD THE EDIT METHOD FOR THE MEETUP
+  edit(data) {
+    // use the getOne method to find the meetup
+    const theMeetup = this.getOne(data.meetupId);
+    const index = this.meetups.indexOf(theMeetup);
+    // Now you can do whatever you so wish on controllers endpoint
+    this.meetups[index].images = data.images || theMeetup.images;
+    this.meetups[index].topic = data.topic || theMeetup.topic;
+    this.meetups[index].location = data.location || theMeetup.location;
+    this.meetups[index].happeningOn = data.happeningOn || theMeetup.happeningOn;
+    this.meetups[index].tags = data.tags || theMeetup.tags;
+  }
 
-	delete(meetupId) {
-		//use getOne method to get the meetup, so let's find the meetup
-		const delMeetup = this.meetups.find(meetup => meetup.meetupId === meetupId);
-		//find index of delMeetup in meetups for use in splice
-		const index = this.meetups.indexOf(delMeetup);
-		//Now splice out!
-		this.meetups.splice(index, 1);
-		return {};
-	}
+  delete(meetupId) {
+    // use getOne method to get the meetup, so let's find the meetup
+    const delMeetup = this.meetups.find(meetup => meetup.meetupId === meetupId);
+    // find index of delMeetup in meetups for use in splice
+    const index = this.meetups.indexOf(delMeetup);
+    // Now splice out!
+    this.meetups.splice(index, 1);
+    return {};
+  }
 }
 
 export default new Meetup();
--- api/v1/models/users.js
+++ api/v1/models/users.js
@@ -3,55 +3,55 @@ import uuid from 'uuid';
 
 
 class User {
-	constructor() {
-		this.users = [];
-	}
+  constructor() {
+    this.users = [];
+  }
 
-	signup(data) {
-		const newUser = {
-			userId: uuid.v4(),
-			registered: moment.now(),
-			firstName: data.firstName,
-			lastName: data.lastName,
-			otherName: data.otherName || '',
-			userName: data.userName,
-			email: data.email,
-			phoneNumber: data.phoneNumber,
-			isAdmin: data.isAdmin,
-			password: data.password,
-			image: data.image||''
-		}
-		this.users.push(newUser);
-		return newUser;
-	}
+  signup(data) {
+    const newUser = {
+      userId: uuid.v4(),
+      registered: moment.now(),
+      firstName: data.firstName,
+      lastName: data.lastName,
+      otherName: data.otherName || '',
+      userName: data.userName,
+      email: data.email,
+      phoneNumber: data.phoneNumber,
+      isAdmin: data.isAdmin,
+      password: data.password,
+      image: data.image || '',
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
 
-	findUser(userId) {
-		return this.users.find(user => user.userId === userId);
-	}
+  findUser(userId) {
+    return this.users.find(user => user.userId === userId);
+  }
 
-	confirm(email) {
-		return this.users.find(user => user.email === email);
-	}
+  confirm(email) {
+    return this.users.find(user => user.email === email);
+  }
 
-	login(data) {
-		const theUser = this.confirm(data.email);
-		if(theUser.email === data.email && theUser.password === data.password) {
-			return theUser;
-		}
-		return 'Not Found';
-	}
+  login(data) {
+    const theUser = this.confirm(data.email);
+    if (theUser.email === data.email && theUser.password === data.password) {
+      return theUser;
+    }
+    return 'Not Found';
+  }
 
-	deleteUser(userId) {
-		const theUser = this.findUser(userId);
-		const index = this.users.IndexOf(theUser);
-		this.users.splice(index, 1);
-	}
+  deleteUser(userId) {
+    const theUser = this.findUser(userId);
+    const index = this.users.IndexOf(theUser);
+    this.users.splice(index, 1);
+  }
 
-	//To be rendered on the profile page
-	userInfor(userId) {
-		const theUser = this.findUser(userId);
-		return theUser;
-	}
+  // To be rendered on the profile page
+  userInfor(userId) {
+    const theUser = this.findUser(userId);
+    return theUser;
+  }
 }
 
 export default new User();
